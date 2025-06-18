499ccf4e37febfeeef972cf5d96b85ae389715e9
Karim <karim.ayoub.job@gmail.com>
2025-06-18 10:53:23 +0200

    Initial commit: Set up API and tests

---
+++ app.js
@@ -0,0 +1,14 @@
+// Mise en place code express API
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/status", (req, res) => {
+  res.json({status: 'API is running'});
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,7 @@
+// Mise en place code express API
+const app = require('./app');
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
